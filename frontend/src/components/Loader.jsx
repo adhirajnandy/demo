a8071f86621fc0aab0d80b9f4d60b1111d8b3761
adhirajnandy <adhirajnandy9@gmail.com>
2024-06-21 13:20:08 +0530

    Modifications in the UI of Loader Components

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,19 +1,29 @@
 import { Spinner } from "react-bootstrap";
-import React from 'react'
+import React from 'react';
 
 const Loader = () => {
   return (
-    <Spinner
+    <div
+      style={{
+        position: 'fixed', // Positioning the loader fixed to the viewport
+        top: '50%', // Centering vertically
+        left: '50%', // Centering horizontally
+        transform: 'translate(-50%, -50%)', // Adjusting for centering effect
+        zIndex: 9999, // Ensuring it appears on top of other content
+      }}
+    >
+      <Spinner
         animation="border"
         role="status"
         style={{
-            width: '100px',
-            height: '100px',
-            margin: 'auto',
-            display:'block',
+          width: '100px',
+          height: '100px',
+          margin: 'auto',
+          display: 'block',
+        }}
+      />
+    </div>
+  );
+};
 
-        }}/>
-  )
-}
-
-export default Loader
+export default Loader;

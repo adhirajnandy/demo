ed6ae98496e7d0317807f8d772be98b18f777e38
adhirajnandy <adhirajnandy9@gmail.com>
2024-06-26 12:50:49 +0530

    [ADD]:Create and Get Orders in the backend

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { 
+    getMyOrders,
+    getOrders,
+    updateOrderToDelivered,
+    updateOrderToPaid,
+    getOrderById,
+    addOrderItems,
+} from '../controllers/orderController.js';
+import {protect, admin} from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route('/').post(addOrderItems).get(protect, admin, getOrders);
+router.route('/myorders').get(protect, getMyOrders);
+router.route('/:id').get(protect,admin, getOrderById);
+router.route('/:id/pay').put(protect,updateOrderToPaid);
+router.route('/:id/deliver').put(protect,admin,updateOrderToDelivered);
+
+
+
+
+export default router;

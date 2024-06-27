a8784c45ac5ac5037f10d31ab89975ba791bfa7a
adhirajnandy <adhirajnandy9@gmail.com>
2024-06-27 17:34:52 +0530

    [UPD]

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -19,7 +19,7 @@ const HomeScreen = () => {
           </Message>
         ) : (
           <>
-            <h1 className='fw-medium'>Latest Products</h1>
+            <h1 className='fw-bolder'>Latest Products</h1>
             <Row>
                 {products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>

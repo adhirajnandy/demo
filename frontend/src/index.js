6a64345d423a577e5e4be696c97d9fd9d4581989
adhirajnandy <adhirajnandy9@gmail.com>
2024-06-28 11:42:56 +0530

    Admin Route and Basic  Functionalities Added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,6 +25,8 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import AdminRoute from './components/AdminRoute';
+import OrderListScreen from './screens/adminScreens/OrderListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -43,6 +45,9 @@ const router = createBrowserRouter(
         <Route path='/order/:id' element={<OrderScreen/>}/>
         <Route path='/profile' element={<ProfileScreen/>}/>
         
+      </Route>
+      <Route path='' element = {<AdminRoute/>}>
+        <Route path='/admin/orderlist' element = {<OrderListScreen />}/>
       </Route>
     </Route>
   )

45d38d6ac557989f6758ef59c43fa21764308eff
adhirajnandy <adhirajnandy9@gmail.com>
2024-07-02 16:28:03 +0530

    User Edit and Delete Functionality Added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -30,6 +30,7 @@ import OrderListScreen from './screens/adminScreens/OrderListScreen';
 import ProductListScreen from './screens/adminScreens/ProductListScreen';
 import ProductEditScreen from './screens/adminScreens/ProductEditScreen';
 import UserListScreen from './screens/adminScreens/UserListScreen';
+import UserEditScreen from './screens/adminScreens/UserEditScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -54,6 +55,7 @@ const router = createBrowserRouter(
         <Route path='/admin/productlist' element = {<ProductListScreen />}/>
         <Route path='/admin/product/:id/edit' element = {<ProductEditScreen/>}/>
         <Route path='/admin/userlist' element = {<UserListScreen/>}/>
+        <Route path='/admin/user/:id/edit' element = {<UserEditScreen />}/>
 
       </Route>
     </Route>

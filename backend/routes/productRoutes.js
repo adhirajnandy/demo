7ada5968850c789908585f70dac1dae20c4de93b
adhirajnandy <adhirajnandy9@gmail.com>
2024-07-03 10:54:25 +0530

    Creating Product Reviews Frontend Added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/').get(getProducts).post(protect,admin,createProduct);
 router.route('/:id').get(getProductById).put(protect,admin,updateProduct).delete(protect,admin,deleteProduct);
-router.route('/:id/reviews').post(protect, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 
 export default router;
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -61,9 +61,17 @@ export const productsApiSlice = apiSlice.injectEndpoints({
 
                 }),
             }
-        ) 
+        ),
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+                method: 'POST',
+                body: data,
+            }),
+            invalidatesTags: ['Product'],
+        }) 
     }), 
         
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation, useCreateReviewMutation } = productsApiSlice;

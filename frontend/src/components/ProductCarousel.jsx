73dc238e804f6ad52d58413e2cc1506c70b8fdec
adhirajnandy <adhirajnandy9@gmail.com>
2024-07-07 00:39:06 +0530

    Product Carousel Update

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -10,9 +10,9 @@ const ProductCarousel = () => {
     const { data: products, isLoading, error } = useGetTopProductsQuery();
   return (
      isLoading ? <Loader/> : error ? <Message variant='danger'>{error?.data?.message}</Message> : (
-        <Carousel pause='hover' className='bg-primary mb-4'>
+        <Carousel pause='hover' className='bg-primary mb-4' interval={2000}>
             {products.map((product) => (
-                <Carousel.Item key={product._id}>
+                <Carousel.Item key={product._id} >
                     <Link to={`/product/${product._id}`}>
                         <Image src={product.image} alt={product.name} fluid/>
                         <Carousel.Caption className='carousel-caption'>

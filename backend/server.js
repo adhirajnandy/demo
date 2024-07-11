3adc610c81b606e9f3f683df3504439e116a5a53
adhirajnandy <adhirajnandy9@gmail.com>
2024-07-12 00:35:58 +0530

    [UPD]

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.use(express.urlencoded({extended: true}));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get('/',(req,res) => {
-    res.send('api is running');
-});
+// app.get('/',(req,res) => {
+//     res.send('api is running');
+// });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
